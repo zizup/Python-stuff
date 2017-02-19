cfff3a4370c6c364e6a7a5d03ed9d0169ad67e0a
DrCode001 <admin@doctorcode.net>
2017-02-19 19:48:42 +0100

    Added TODO list

--- trikotniki-clean3.py
+++ trikotniki-clean3.py
@@ -1,10 +1,8 @@
 ###############################################################################
 #	trikotniki-clean3.py
 #	Added separate functions to do stuff
-#	13 February 2017
-# 	New functions:
-#	- https://docs.python.org/2/library/datetime.html#datetime.datetime.utcnow
-#	- http://stackoverflow.com/questions/19192209/attributeerror-module-object-has-no-attribute-utcnow
+#	19 February 2017
+# 	New TODO stuff
 #	Usage: time python trikotniki-clean3.py trikotniki.txt
 ###############################################################################
 from math import *
@@ -12,11 +10,12 @@ from sys import argv
 from datetime import datetime
 
 def main():
-	script, filename = argv
+	script, filename = argv # script=trikotniki-clean3.py, filename=trikotniki.txt
 
 	max_search = take_user_input()
-	successful_results = calculate(max_search)
-	check_results(successful_results)
+	successful_results = calculate(max_search) # calculate() returns an array
+	# and stores it in successful_results[]
+	check_results(successful_results) # checks the array calculate() returned
 	print_sorted_results(successful_results, filename)
 
 
@@ -39,7 +38,7 @@ def take_user_input():
 
 
 def calculate(max_search):
-	successful_results = []
+	successful_results_for_calculate_function = [] # calculate function returns this array
 	combinations, success, fail = 0, 0, 0 
 	print "CALCULATING ..."
 	for a in range(1,max_search + 1): # TODO replace it with xrange()
@@ -59,48 +58,57 @@ def calculate(max_search):
 			
 			if c % int(c) == 0: # this checks for an integer
 				success += 1
-				tmp_array = [c, a, b]
-				successful_results.append(tmp_array)
+				tmp_array = [c, a, b] 
+				successful_results_for_calculate_function.append(tmp_array)
 			else:
 				fail += 1
 
 	print "combinations tried out:", combinations
-	print "successes:", success # TODO: replace these  
-	# with print "successes: {0}" . format(success), because the script runs faster
+	print "successes:", success # TODO: replace these with 'print "successes: {0}" . format(success)'
+	# because the script runs faster
 	print "fails:", fail
-	# print "successful_results:", successful_results
+	# print "successful_results_for_calculate_function:", successful_results_for_calculate_function
 	print "-" * 20
-	return successful_results
+	return successful_results_for_calculate_function
+	return success # TODO: can a function return > 1 outcome? I need this for check_result() TODO
 
 
-def check_results(successful_results):
-	print "CHECKING ..." # CHECKING
-	for results in successful_results:
-		for check_again in successful_results:
+def check_results(array_to_be_checked): #checks the array calculate() returned
+	print "CHECKING ..." # CHECKING for the same triangles (a=3,b=4 is the
+	# same triangle as a=4,b=3)
+	for results in array_to_be_checked:
+		for check_again in array_to_be_checked:
 			# delete doubles
 			if results[0] == check_again[0]: 
 				if results[1] == check_again[2] and results[2] == check_again[1]:
-					successful_results.remove(check_again)
+					array_to_be_checked.remove(check_again)
 	print "CHECKED!"
+
+	"""TODO: print again how many combinations there are left, and compare them to
+	the variable 'success' (a=3,b=4,c=5 and a=4,b=3,c=5 are considered as)
+	two different triangles at 'success' variable"""
+	print "Length:", len(array_to_be_checked)
+
+
 	print "-" * 20
 
 
-def print_sorted_results(successful_results, filename):
+def print_sorted_results(array_to_be_printed, file_to_be_opened):
 	# Print sorted results to a file "trikotniki.txt"
-	# The lambda returns the first element of each of the inner lists and the
-	# sorted function uses that to sort those list.
-	successful_results = sorted(successful_results, key = lambda x: x[0])
-	target_file = open(filename, 'w')
+	"""The lambda returns the first element of each of the inner lists and the
+	sorted function uses that to sort those list. The first element in our case is
+	c; see 'tmp_array'."""
+	array_to_be_printed = sorted(array_to_be_printed, key = lambda x: x[0])
+	target_file = open(file_to_be_opened, 'w') # open file with write privileges
 	# write a date
 	target_file.write(str(datetime.utcnow()))
 	
 	target_file.write("\nPythagorean theorem: c = sqrt(a**2 + b**2)\nLegend: c, a, b\n\n")
 	
-	print "END RESULT --> see %s" % filename
-	for end_result in successful_results:
-		target_file.write(str(end_result) + "\n")
+	print "END RESULT --> see %s" % file_to_be_opened
+	for each_result in array_to_be_printed:
+		target_file.write(str(each_result) + "\n")
 	target_file.close()
-
 
 if __name__ == "__main__":
 	main()
